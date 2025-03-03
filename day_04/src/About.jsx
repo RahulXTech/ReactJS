ddc335c188ebca92b4cc1b59c32fa70d4d4aab0b
rahulkumarclg <rahul.code2024@gmail.com>
2025-03-03 20:32:06 +0530

    this is third day

---
+++ day_04/src/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function About(props) {
+  console.log(props.email)
+  console.log(props.address)
+  console.log(props.college.subject)
+  return (
+    <div>
+      <h1>Hello props</h1>
+      <h3>{props.address}</h3>
+      <h5>{props.college.subject}</h5>
+    </div>
+  )
+}
+
+export default About
+
---
+++ day_04/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './About';
+
+
+function App() {
+  let student = {
+    name:"rahul_kumar",
+    branch:"cse",
+    subject:"computer_science"
+  }
+  return (
+    <div>
+      <About email="rahulkumar@gmail.com" address="kiran nagar bhopal madhya pradesh india" college={student} />
+    </div>
+  );
+}
+
+export default App;
