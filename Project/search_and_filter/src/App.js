eab18c7d6557a75944a2174dbdeaec1f8131bc69
rahulkumarclg <rahul.code2024@gmail.com>
2025-03-12 20:47:27 +0530

    Today, I worked on project

--- Project/search_and_filter/src/App.js
+++ Project/search_and_filter/src/App.js
@@ -1,19 +1,35 @@
 import Category from "./Category";
-
+import Product from "./Product";
 
 function App() {
   return (
     <>
-        <div className='py-[40px]'>
-                <div className=' max-w-[1320px] mx-auto'>
-                  <h1 className="text-center text-[40px] font-bold mb-[30px]">Our Procucts</h1>
-                    <div className='grid grid-cols-[30%_auto] gap-[20px]'>
-                        <div className='bg-gray-200'>
-                            <Category/>
-                        </div>
-                    </div>
-                </div> 
+      <div className="py-[40px]">
+        <div className=" max-w-[1320px] mx-auto">
+          <h1 className="text-center text-[40px] font-bold mb-[30px]">
+            Our Procucts
+          </h1>
+          <div className="grid grid-cols-[30%_auto] gap-[20px]">
+            <div className="bg-gray-200">
+              <Category />
+            </div>
+            <div>
+              <div className="grid grid-cols-3 gap-4">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </>
   );
 }
---
+++ Project/search_and_filter/src/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div className="shadow-lg text-center pb-4 hover:bg-slate-300">
+      <img className='cursor-pointer  hover:size-full touch-pinch-zoom' src="https://media.ldlc.com/r1600/ld/products/00/05/97/76/LD0005977695_0005977720_0005977750.jpg"></img>
+      <p>Price</p>
+      <p>$2000</p>
+    </div>
+  );
+}
+
+export default Product
