71ceb6284d8a9959faf65c8909e72c65375e4e08
rahulkumarclg <rahul.code2024@gmail.com>
2025-03-30 14:28:53 +0530

    Rahul

--- Project/ToDoList/src/App.jsx
+++ Project/ToDoList/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
-import TodoList from './TodoList'
-
 function App() {
   return (
     <>
-    <h1>Hello..........</h1>
-      <TodoList/>
+    <center>
+    <div class="container text-center">
+  <div class="row">
+    <div class="col-4"><input type="text" placeholder="Enter the Todo massage" /></div>
+    <div class="col-5"><input type="date" /></div>
+    <div class="col-2">
+    <button type="button" class="btn btn-success">Success</button>
+    </div>
+  </div>
+</div>
+     </center>
     </>
   )
 }
-
 export default App
