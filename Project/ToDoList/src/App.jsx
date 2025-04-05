844e7545c69cfd96c4fed40428bb66dc3c52dc5a
rahulkumarclg <rahul.code2024@gmail.com>
2025-04-05 20:53:05 +0530

    rahul

--- Project/ToDoList/src/App.jsx
+++ Project/ToDoList/src/App.jsx
@@ -1,18 +1,70 @@
+import "./App.css";
+import { useState, useEffect } from "react";
 function App() {
+  let [inputvalues, setInputvalues] = useState([]);
+  let [add, setAdd] = useState(null);
+  
+  const inputChange = (e)=>{
+    let toname = setInputvalues(e.target.value);
+    if(!inputvalues.include(toname)){
+        let finalList = [...todolist, toname]
+    }
+    else{ 
+    alert("Eror dublicate entry");
+    console.log(e.target.value);
+    }
+  };
+  
+  let printingss = () => {
+    console.log("button is working");
+    console.log(inputvalues)
+    setAdd();
+  };
+  let TodoItems = [
+    { item: "Buy milk", date: "31/03/2025" },
+    { item: "Goto library", date: "31/03/2025" },
+  ];
+
   return (
     <>
-    <center>
-    <div class="container text-center">
-  <div class="row">
-    <div class="col-4"><input type="text" placeholder="Enter the Todo massage" /></div>
-    <div class="col-5"><input type="date" /></div>
-    <div class="col-2">
-    <button type="button" class="btn btn-success">Success</button>
-    </div>
-  </div>
-</div>
-     </center>
+      <center>
+        <h1 className="">TODO Application</h1>
+        <div className="container text-center">
+          <div className="row">
+            <div className="col-4">
+              <input type="text" placeholder="Enter massage"
+               onChange={inputChange}/>
+            </div>
+            <div className="col-6">
+              <input type="date" width="2vw" />
+            </div>
+            <div className="col-1">
+              <button
+                onClick={()=>{
+                  printingss();}}
+                type="button"
+                className="btn btn-success">
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
+        <ul className="container text-center">
+          <li className="row">
+
+            <div className="col-4">
+              {inputvalues}
+            </div>
+            <div className="col-6">04/10/2025</div>
+            <div className="col-1">
+              <button type="button" className="btn btn-danger">
+                Delete
+              </button>
+            </div>
+          </li>
+        </ul>
+      </center>
     </>
-  )
+  );
 }
-export default App
+export default App;
